1664b7d657398b39b2ab0d31fa182f25d2212a28
Shuoran Liu <shuoran.liu@smartx.com>
2023-04-27 12:07:00 +0800

    feat: introduce sfs cluster recovery backend
    Signed-off-by: Shuoran Liu <shuoran.liu@smartx.com>

--- src/support/nfs_read_conf.c
+++ src/support/nfs_read_conf.c
@@ -397,6 +397,7 @@ static struct config_item_list recovery_backend_types[] = {
 	CONFIG_LIST_TOK("rados_kv",		RECOVERY_BACKEND_RADOS_KV),
 	CONFIG_LIST_TOK("rados_ng",		RECOVERY_BACKEND_RADOS_NG),
 	CONFIG_LIST_TOK("rados_cluster",	RECOVERY_BACKEND_RADOS_CLUSTER),
+	CONFIG_LIST_TOK("sfs_cluster",		RECOVERY_BACKEND_SFS_CLUSTER),
 	CONFIG_LIST_EOL
 };
 
