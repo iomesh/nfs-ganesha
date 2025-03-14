1a095d02e378e008b08503cf139bc6b91606a887
zhitaoli <zhitao.li@iomesh.com>
2025-03-14 17:17:48 +0800

    feat(build): support `sfs_only` build config
    Change-Id: I3722f222a683403db5c54a6d354a0fa3c9d622f9
    Signed-off-by: zhitaoli <zhitao.li@iomesh.com>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -119,6 +119,7 @@
 #cmakedefine USE_BTRFSUTIL 1
 #cmakedefine USE_MONITORING 1
 #cmakedefine USE_MINITRACE 1
+#cmakedefine ENABLE_SFS 1
 #define NFS_GANESHA 1
 
 #define GANESHA_CONFIG_PATH "@SYSCONFDIR@/ganesha/ganesha.conf"
