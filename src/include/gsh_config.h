d2be9c3a0351c0a53f99f43884c3fcc2898ba00f
Guoliang Jiang <guoliang.jiang@iomesh.com>
2025-12-19 18:48:23 +0800

    feat: add allow_back_channel paramter
    Signed-off-by: Guoliang Jiang <guoliang.jiang@iomesh.com>

--- src/include/gsh_config.h
+++ src/include/gsh_config.h
@@ -606,6 +606,8 @@ typedef struct nfs_version4_parameter {
 	bool enforce_utf8_vld;
 	/** Max number of Client IDs allowed on the system */
 	uint32_t max_client_ids;
+	/** allow create channel by create seesion operation **/
+	bool allow_back_channel;
 } nfs_version4_parameter_t;
 
 /** @} */
